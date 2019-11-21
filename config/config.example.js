468fb16adc05564b332bfcefb83220ff8e0be369
Eduardo Neves <neves98@outlook.com>
2019-11-21 11:58:44 -0300

    master: alter readme.md

---
+++ config/config.example.js
@@ -0,0 +1,24 @@
+module.exports = {
+  secret: 'codesecrettotvs',
+  development: {
+    username: "postgres",
+    passwor: null,
+    database: "ecommerce",
+    host: "127.0.0.1",
+    dialect: "postgresql"
+  },
+  test: {
+    username: "postgres",
+    passwor: null,
+    database: "ecommerce",
+    host: "127.0.0.1",
+    dialect: "postgresql"
+  },
+  production: {
+    username: "postgres",
+    passwor: null,
+    database: "ecommerce",
+    host: "127.0.0.1",
+    dialect: "postgresql"
+  }
+}
